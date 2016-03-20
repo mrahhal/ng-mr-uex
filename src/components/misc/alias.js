42d278d0e9f9c9c26c3ef82646bf405a98d3c0a7
Mohammad Rahhal <m.r992@hotmail.com>
2016-03-20 14:27:55 +0200

    + uexAlias

---
+++ src/components/misc/alias.js
@@ -0,0 +1,23 @@
+(function () {
+	'use strict';
+
+	angular.module('mr.uex').directive('uexAlias', uexAlias);
+
+	function uexAlias() {
+		return {
+			restrict: 'A',
+			link: function ($scope, $element, $attrs) {
+				var expr = $attrs.uexAlias,
+					parts = expr.split(' '),
+					source = parts[0],
+					dest = parts[1];
+
+				$scope.$watch(function () {
+					return $scope.$eval(source);
+				}, function (value) {
+					$scope[dest] = value;
+				});
+			}
+		};
+	}
+})();
