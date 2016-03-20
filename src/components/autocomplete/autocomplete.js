e705466d315d05845bf514b93a6ce3abbda0d311
Mohammad Rahhal <m.r992@hotmail.com>
2016-03-20 14:29:22 +0200

    + uexAutocomplete

---
+++ src/components/autocomplete/autocomplete.js
@@ -0,0 +1,205 @@
+(function () {
+	'use strict';
+
+	angular.module('mr.uex')
+		.directive('uexAutocomplete', uexAutocomplete);
+
+	function uexAutocompleteCtrl($scope, $attrs, $parse, $q) {
+		function parse(exp) {
+			var match = exp.match(/^\s*([\s\S]+?)\s+in\s+([\s\S]+?)(?:\s+as\s+([\s\S]+?))?\s*$/);
+
+			return {
+				keyName: match[1],
+				inFn: $parse(match[2]),
+				asFn: $parse(match[3])
+			};
+		}
+
+		if ($attrs.exp === undefined) {
+			throw new Error('\'uexAutocomplete\': Attribute \'exp\' is required.');
+		}
+
+		var ctrl = this,
+			options = parse($attrs.exp),
+			keyName = options.keyName,
+			promise;
+
+		ctrl.items = [];
+		ctrl.text = [];
+		ctrl.options = options;
+		ctrl.keyName = keyName;
+		ctrl.activeItem = null;
+		ctrl.activeIndex = -1;
+
+		var transient = false;
+
+		ctrl.display = function (item) {
+			if (options.asFn === angular.noop) return item;
+			var locals = {};
+			locals[keyName] = item;
+			return options.asFn($scope, locals);
+		};
+
+		ctrl.select = function (item) {
+			ctrl.text = ctrl.display(item);
+			ctrl.clear();
+			transient = true;
+		};
+
+		ctrl.setActive = function (index) {
+			if (index === undefined) {
+				ctrl.activeItem = null;
+				ctrl.activeIndex = -1;
+				return;
+			}
+			var item = ctrl.items[index];
+
+			ctrl.activeItem = item;
+			ctrl.activeIndex = index;
+		};
+
+		ctrl.mouseover = function (item, index) {
+			ctrl.setActive(index);
+		};
+
+		ctrl.clear = function () {
+			ctrl.items = [];
+			ctrl.setActive();
+		};
+
+		function filterIfNotPromise(o) {
+			if (o.then) return o;
+			var text = ctrl.text;
+			if (!text || text.trim() === '') return o;
+			var r = [];
+			for (var i = 0; i < o.length; i++) {
+				if (ctrl.display(o[i]).indexOf(text) > -1) {
+					r.push(o[i]);
+				}
+			}
+			return r;
+		}
+
+		$scope.$watch(function () {
+			return ctrl.text;
+		}, function watchText(v, old) {
+			if (v === old || v === null || transient) {
+				transient = false;
+				return;
+			}
+			ctrl.ngModel.$setViewValue(v);
+			ctrl.loading = true;
+			ctrl.clear();
+			var p = promise = $q.when(filterIfNotPromise(ctrl.options.inFn($scope, { // jshint ignore:line
+				q: v
+			})));
+			p.then(function (d) {
+				if (p !== promise) return;
+				ctrl.items = d;
+			}).finally(function () {
+				ctrl.loading = false;
+			});
+		});
+	}
+
+	function uexAutocomplete($document) {
+		return {
+			restrict: 'E',
+			controller: uexAutocompleteCtrl,
+			controllerAs: '$uexAutocompleteCtrl',
+			template: function (element, attr) {
+				function getItemTemplate() {
+					var templateTag = element.find('uex-item-template').detach(),
+						html = templateTag.length ? templateTag.html() : element.html();
+					if (!templateTag.length) element.empty();
+					return html;
+				}
+				return '\
+					<div class="uex-autocomplete">\
+						<input type="text" ng-model="$uexAutocompleteCtrl.text" ng-keydown="keydown($event)" >\
+						<div class="uex-autocomplete-list" ng-if="$uexAutocompleteCtrl.items.length > 0">\
+							<div class="uex-autocomplete-item"\
+								 ng-repeat="item in $uexAutocompleteCtrl.items"\
+								 ng-click="$uexAutocompleteCtrl.select(item)"\
+								 ng-class="{ active: $index == $uexAutocompleteCtrl.activeIndex }"\
+								 ng-mouseover="$uexAutocompleteCtrl.mouseover(item, $index)"\
+								 uex-alias="item {{::$uexAutocompleteCtrl.keyName}}">' + getItemTemplate() + '\
+							</div>\
+						</div>\
+					</div>';
+			},
+			require: ['uexAutocomplete', 'ngModel'],
+			scope: true,
+			link: function ($scope, $element, $attrs, ctrls, $transclude) {
+				var ctrl = ctrls[0],
+					ngModel = ctrls[1];
+
+				ctrl.ngModel = ngModel;
+
+				ngModel.$render = function () {
+					ctrl.text = ngModel.$viewValue;
+				};
+
+				$scope.keydown = function (e) {
+					var key = e.which,
+						shouldPreventDefault = true;
+
+					switch (key) {
+						case 13: // enter
+							ctrl.select(ctrl.activeItem);
+							break;
+
+						case 27: // esc
+							ctrl.clear();
+							break;
+
+						case 38: // up
+							if (ctrl.items.length === 0) break;
+							if (ctrl.activeIndex === -1) {
+								ctrl.setActive(ctrl.items.length - 1);
+								break;
+							}
+							if (ctrl.activeIndex - 1 < 0) break;
+							ctrl.setActive(ctrl.activeIndex - 1);
+							break;
+
+						case 40: // down
+							if (ctrl.items.length === 0) break;
+							if (ctrl.activeIndex === -1) {
+								ctrl.setActive(0);
+								break;
+							}
+							if (ctrl.activeIndex + 1 >= ctrl.items.length) break;
+							ctrl.setActive(ctrl.activeIndex + 1);
+							break;
+
+						default:
+							shouldPreventDefault = false;
+							break;
+					}
+
+					if (shouldPreventDefault) {
+						e.preventDefault();
+					}
+				};
+
+				$element.on('keydown', function (e) {
+					if (e.which === 27) {
+						e.preventDefault();
+						$scope.$apply(function () {
+							ctrl.clear();
+						});
+					}
+				});
+
+				$document.on('click', function (e) {
+					if (!$.contains($element[0], e.target)) {
+						$scope.$apply(function () {
+							ctrl.clear();
+						});
+					}
+				});
+			}
+		};
+	}
+})();
---
+++ src/components/autocomplete/demo/demo.js
@@ -0,0 +1,26 @@
+(function () {
+	'use strict';
+
+	angular.module('app').controller('uexAutocompleteCtrl', function ($scope, $timeout, $q) {
+		$scope.items = [{
+			name: 'item1'
+		}, {
+			name: 'item2'
+		}];
+
+		this.items = $scope.items;
+
+		$scope.getItems = function (q) {
+			return $timeout(function () {
+				var items = [];
+				for (var i = 0; i < $scope.items.length; i++) {
+					if (!q || $scope.items[i].name.indexOf(q) > -1) {
+						items.push($scope.items[i]);
+					}
+				}
+				return items;
+			}, 2000);
+		};
+	});
+
+})();
