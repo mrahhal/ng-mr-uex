7bf71a635af009f969de5316a31dd83d3993d001
Mohammad Rahhal <m.r992@hotmail.com>
2016-03-20 14:33:23 +0200

    + uexTooltip

---
+++ src/components/tooltip/tooltip.js
@@ -0,0 +1,31 @@
+(function () {
+	'use strict';
+
+	angular.module('mr.uex').directive('uexTooltip', uexTooltip);
+
+	function uexTooltip() {
+		function extractPlacement(v) {
+			var index = v.indexOf(',');
+			return v.slice(0, index).trim();
+		}
+
+		function extractText(v) {
+			var index = v.indexOf(',');
+			return v.slice(index + 1).trim();
+		}
+
+		return {
+			restrict: 'A',
+			scope: false,
+			link: function ($scope, $element, $attrs) {
+				var placement = extractPlacement($attrs.uexTooltip);
+				$element.addClass('tooltipped tooltipped-' + placement);
+
+				$attrs.$observe('uexTooltip', function (v) {
+					var text = extractText(v);
+					$element.attr('aria-label', text);
+				});
+			}
+		};
+	}
+})();
