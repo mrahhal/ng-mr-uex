0bab593cad998b97345bd58ff1bb42933d8e377f
Mohammad Rahhal <m.r992@hotmail.com>
2016-03-20 14:28:06 +0200

    + uexFocus

---
+++ src/components/misc/focus.js
@@ -0,0 +1,18 @@
+(function () {
+	'use strict';
+
+	angular.module('mr.uex').directive('uexFocus', uexFocus);
+
+	function uexFocus($timeout) {
+		return {
+			restrict: 'A',
+			link: function ($scope, $element, $attrs) {
+				$scope.$on('uex.focus', function () {
+					$timeout(function () {
+						$element.focus();
+					});
+				});
+			}
+		};
+	}
+})();
