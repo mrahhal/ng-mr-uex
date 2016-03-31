3ec690d13f2390999daa7b606d3ba7e826d6b7d2
Mohammad Rahhal <m.r992@hotmail.com>
2016-03-31 21:16:40 +0300

    dist

--- dist/ng-mr-uex.js
+++ dist/ng-mr-uex.js
@@ -119,18 +119,19 @@ angular
 					return html;
 				}
 				return '\
-					<div class="uex-autocomplete">\
-						<input type="text" ng-model="$uexAutocompleteCtrl.text" ng-keydown="keydown($event)" >\
-						<div class="uex-autocomplete-list" ng-if="$uexAutocompleteCtrl.items.length > 0">\
-							<div class="uex-autocomplete-item"\
-								 ng-repeat="item in $uexAutocompleteCtrl.items"\
-								 ng-click="$uexAutocompleteCtrl.select(item)"\
-								 ng-class="{ active: $index == $uexAutocompleteCtrl.activeIndex }"\
-								 ng-mouseover="$uexAutocompleteCtrl.mouseover(item, $index)"\
-								 uex-alias="item {{::$uexAutocompleteCtrl.keyName}}">' + getItemTemplate() + '\
-							</div>\
-						</div>\
-					</div>';
+<div class="uex-autocomplete">\
+	<input type="text" ng-model="$uexAutocompleteCtrl.text" ng-keydown="keydown($event)" >\
+	<div class="uex-autocomplete-list" ng-if="$uexAutocompleteCtrl.items.length > 0">\
+		<div class="uex-autocomplete-item"\
+			 ng-repeat="item in $uexAutocompleteCtrl.items"\
+			 ng-click="$uexAutocompleteCtrl.select(item)"\
+			 ng-class="{ active: $index == $uexAutocompleteCtrl.activeIndex }"\
+			 ng-mouseover="$uexAutocompleteCtrl.mouseover(item, $index)"\
+			 uex-alias="item {{::$uexAutocompleteCtrl.keyName}}">' +
+			 getItemTemplate() + '\
+		</div>\
+	</div>\
+</div>';
 			},
 			require: ['uexAutocomplete', 'ngModel'],
 			scope: true,
@@ -392,6 +393,102 @@ angular
 (function () {
 	'use strict';
 
+	angular.module('mr.uex').directive('uexP', uexP);
+	angular.module('mr.uex').directive('uexPSrc', uexPSrc);
+
+	function uexP($parse) {
+		return {
+			restrict: 'A',
+			scope: true,
+			controller: controller,
+			controllerAs: '$p',
+			link: link
+		};
+
+		function controller($scope, $timeout, $q) {
+			var ctrl = this,
+				promise;
+
+			this.$$fn = null;
+			this.$running = false;
+			this.$$promises = {};
+
+			var running = function (value) {
+				ctrl.$running = value;
+			};
+
+			function getLocals(args) {
+				if (!args || args.length === 0) {
+					return null;
+				}
+				return {
+					$event: args[0]
+				};
+			}
+
+			var interpolate = function(name, interval) {
+				ctrl[name] = true;
+				var p = ctrl.$$promises[name] = $timeout(function () {
+					if (ctrl.$$promises[name] === p) {
+						ctrl[name] = false;
+					}
+				}, interval);
+			};
+
+			this.run = function () {
+				var p = ctrl.$$fn($scope, getLocals(arguments));
+				if (p && p.finally) {
+					promise = p;
+					running(true);
+					promise.then(function () {
+						interpolate('$success', ctrl.$$options.successInterval || 1000);
+					}, function () {
+						interpolate('$error', ctrl.$$options.errorInterval || 1000);
+					});
+					promise.finally(function () {
+						if (p !== promise) return;
+						running(false);
+					});
+				}
+			};
+		}
+
+		function link($scope, $element, $attrs, ctrl) {
+			ctrl.$$fn = $parse($attrs.uexP);
+			ctrl.$$options = $scope.$eval($attrs.uexPOptions) || {};
+
+			if ($element.is('form') && $attrs.uexPSrc === undefined) {
+				$element.on('submit', function (e) {
+					$scope.$apply(ctrl.run.call(ctrl, e));
+				});
+			}
+		}
+	}
+
+	function uexPSrc() {
+		function determineEvent($element, value) {
+			if (value && angular.isString(value)) return value;
+			if ($element.is('form')) return 'submit';
+			return 'click';
+		}
+
+		return {
+			restrict: 'A',
+			require: '^uexP',
+			scope: false,
+			link: function ($scope, $element, $attrs, ctrl) {
+				var event = determineEvent($element, $attrs.uexPSrc);
+				$element.on(event, function (e) {
+					$scope.$apply(ctrl.run.call(ctrl, e));
+				});
+			}
+		};
+	}
+})();
+
+(function () {
+	'use strict';
+
 	angular.module('mr.uex')
 		.directive('uexSelectTransclude', uexSelectTransclude)
 		.directive('uexSelect', uexSelect)
@@ -463,11 +560,12 @@ angular
 	</div>\
 </div>',
 			controller: function ($scope) {
+				var ctrl = this;
 				var scopes = [];
 				this.$populateScope = function (scope) {
 					var item = scope.item;
 					scopes.push(scope);
-					if (item && item === $scope.selected) {
+					if (item && ctrl.track(item) === ctrl.track($scope.selected)) {
 						scope.$selected = true;
 					} else if (item) {
 						scope.$selected = false;
@@ -531,7 +629,7 @@ angular
 					return options.asFn($scope, locals);
 				};
 
-				var track = function (item) {
+				var track = ctrl.track = function (item) {
 					if (options.trackFn === angular.noop) return item;
 					var locals = {};
 					locals[keyName] = item;
