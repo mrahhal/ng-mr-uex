1ec931cb2402dc6a662442aa252f1b1cf51163dd
Mohammad Rahhal <m.r992@hotmail.com>
2016-11-07 10:25:03 +0200

    modal: + uex-modal-confirm directive

--- src/components/modal/modalDirective.js
+++ src/components/modal/modalDirective.js
@@ -3,12 +3,12 @@
 
 	angular
 		.module('mr.uex')
-		.directive('uexModal', modal);
+		.directive('uexModal', modal)
+		.directive('uexModalConfirm', modalConfirm);
 
 	function modal(uexModal) {
 		return {
 			restrict: 'E',
-			terminal: true,
 			scope: true,
 			bindToController: {
 				delegate: '='
@@ -27,6 +27,37 @@
 							class: classes,
 							template: template
 						}, options));
+					}
+				};
+
+				this.$postLink = () => {
+					$element.removeClass();
+					$element.empty();
+				};
+			}
+		};
+	}
+
+	function modalConfirm(uexModal) {
+		return {
+			restrict: 'E',
+			scope: true,
+			bindToController: {
+				delegate: '='
+			},
+			controllerAs: '$uexModalConfirmCtrl',
+			controller: function ($scope, $element, $attrs) {
+				var title = $attrs.title,
+					classes = $attrs.class,
+					template = $element.html();
+
+				this.delegate = {
+					open: () => {
+						return uexModal.confirm()
+							.classes(classes)
+							.title(title)
+							.template(template)
+							.open($scope);
 					}
 				};
 
